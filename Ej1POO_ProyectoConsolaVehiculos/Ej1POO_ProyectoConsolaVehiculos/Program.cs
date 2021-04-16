42a43d66f3762240b4bea17d6d5bb075595df061
Juan Pablo <jppb1982@gmail.com>
2021-04-16 00:26:28 -0300

    Primer comit de prueba

---
+++ Ej1POO_ProyectoConsolaVehiculos/Ej1POO_ProyectoConsolaVehiculos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ej1POO_ProyectoConsolaVehiculos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!!");
+        }
+    }
+}
