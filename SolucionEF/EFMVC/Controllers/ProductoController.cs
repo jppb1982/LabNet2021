5a0c2339ce4529b1bfd0acacb30d683579ea5085
Juan Pablo <jppb1982@gmail.com>
2021-05-08 19:49:43 -0300

    Primera instancia Listado Productos

---
+++ SolucionEF/EFMVC/Controllers/ProductoController.cs
@@ -0,0 +1,99 @@
+﻿using EFCapaLogica;
+using EFEntities;
+using EFMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace EFMVC.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            LogicaProductos logicaProductos = new LogicaProductos();
+            List<Products> listaProductos = logicaProductos.ObtenerTodos();
+            List<ProductoView> listaVistaProducto = listaProductos.Select(p => new ProductoView
+            {
+                Id = p.ProductID,
+                Nombre = p.ProductName,
+                CantidadPorUnidad = p.QuantityPerUnit,
+                PrecioUnitario = (decimal)p.UnitPrice
+            }).ToList();
+            return View(listaVistaProducto);
+        }
+
+        // GET: Producto/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Producto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Producto/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Producto/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Producto/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Producto/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Producto/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SolucionEF/EFMVC/Models/ProductoView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EFMVC.Models
+{
+    public class ProductoView
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String CantidadPorUnidad { get; set; }
+        public Decimal PrecioUnitario { get; set; }
+
+    }
+}
