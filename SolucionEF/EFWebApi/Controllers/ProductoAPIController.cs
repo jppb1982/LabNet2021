4f49fcd72917cc1e392b8c3185fe09b9f1d4a1de
Juan Pablo <jppb1982@gmail.com>
2021-05-16 21:22:50 -0300

    Cors

--- SolucionEF/EFWebApi/Controllers/ProductoAPIController.cs
+++ SolucionEF/EFWebApi/Controllers/ProductoAPIController.cs
@@ -8,10 +8,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 
 namespace EFWebApi.Controllers
 {
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
     public class ProductoAPIController : ApiController
     {
 
